10acc0ab0c45ce88d24c71d22f160f7bcacc2f40
Masashi <hikozaru.kinopio@gmail.com>
2017-11-23 15:52:25 +0900

    change app.js

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 /* 2. listen()メソッドを実行して3000番ポートで待ち受け。*/
-var server = app.listen(3000, function(){
+var server = app.listen(function(){
     console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
